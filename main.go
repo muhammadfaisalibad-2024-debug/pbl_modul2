fc1eb688d69ef3f26603048b0823c1e46e38cde8
muhammadfaisalibad-2024-debug <muhammad.faisal.ibad-2024@vokasi.unair.ac.id>
2026-08-23 23:03:07 +0700

    add content type validation

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Method() == fiber.MethodPost ||
+			c.Method() == fiber.MethodPut ||
+			c.Method() == fiber.MethodPatch {
+
+			contentType := c.Get("Content-Type")
+
+			if !strings.HasPrefix(contentType, fiber.MIMEApplicationJSON) {
+				return c.Status(fiber.StatusUnsupportedMediaType).JSON(fiber.Map{
+					"success": false,
+					"message": "Content-Type must be application/json",
+				})
+			}
+		}
+
+		return c.Next()
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("API Students")
