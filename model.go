6282a640db7bc05aca31a3315038cb9eee09381c
muhammadfaisalibad-2024-debug <muhammad.faisal.ibad-2024@vokasi.unair.ac.id>
2026-08-23 18:06:38 +0700

    student models

---
+++ model.go
@@ -0,0 +1,30 @@
+package main
+
+type Student struct {
+	ID       int     `json:"id"`
+	NIM      string  `json:"nim"`
+	Name     string  `json:"name"`
+	Grade    float64 `json:"grade"`
+	IsActive bool    `json:"is_active"`
+}
+
+type CreateStudentRequest struct {
+	NIM      string  `json:"nim"`
+	Name     string  `json:"name"`
+	Grade    float64 `json:"grade"`
+	IsActive bool    `json:"is_active"`
+}
+
+type ReplaceStudentRequest struct {
+	NIM      string  `json:"nim"`
+	Name     string  `json:"name"`
+	Grade    float64 `json:"grade"`
+	IsActive bool    `json:"is_active"`
+}
+
+type PatchStudentRequest struct {
+	NIM      *string  `json:"nim,omitempty"`
+	Name     *string  `json:"name,omitempty"`
+	Grade    *float64 `json:"grade,omitempty"`
+	IsActive *bool    `json:"is_active,omitempty"`
+}
