d7b6f30e470908b255445ec263afab6035da2f9d
muhammadfaisalibad-2024-debug <muhammad.faisal.ibad-2024@vokasi.unair.ac.id>
2026-08-30 12:33:10 +0700

    configuration

---
+++ config/env.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppPort    string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+	DBMaxConns string
+}
+
+func Load() Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found, using environment variables")
+	}
+
+	return Config{
+		AppPort:    os.Getenv("APP_PORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBSSLMode:  os.Getenv("DB_SSLMODE"),
+		DBMaxConns: os.Getenv("DB_MAX_CONNS"),
+	}
+}
